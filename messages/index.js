c3f8526132c1e7a17d9ed84a29220e839714f765
Thiago Moreira <thiago.cmoreira@yahoo.com.br>
2020-07-12 14:32:47 -0300

    :art: Updating any messages

--- messages/index.js
+++ messages/index.js
@@ -1,7 +1,7 @@
 let pitMessages = [
-  'Pitanga ta cansaaaaadoon passou o dia todo na ruuuuan',
-  'Pitanga ta cansaaaaadoon tava na pós graduaçaummm',
-  'Pitanga ta no trabalhooon ta cansaaaadon',
+  'Pitanga ta cansaaaaado passou o dia todo na ruuuua',
+  'Pitanga ta cansaaaaado tava na pós graduaçaummm',
+  'Pitanga ta no trabalhooo ta cansaaaado',
   'Po meu velho, não vai rolar, to na pós',
   'Rusha meio e sobe caminho de rato'
 ]
@@ -10,24 +10,22 @@ let bntoMessages = [
   'Boto fé, boto fé',
   'Pode pá, pode pá',
   'Tranquilo, tranquilo',
-  'Ezzzzz baiiiiiit',
   'Ta xitaaaaado',
-  '💩🍆'
+  'Oi?',
+  'Hã?'
 ]
 
 let donatoMessages = [
   'Abriram as inscrições pro vestibular http://www.cespe.unb.br/vestibular/VESTUNB_14_2/',
   'GG mano, já era',
-  '🚬🍁💨🤤',
-  'Drogas? 🖐🖐🖐 Quero 5! 🤪'
+  '🚬🍁💨'
 ]
 
 let paoMessages = [
   'Retakes milhonaaaarios',
-  'Vocês são muito lacreiros',
-  '👉🤪👉🇧🇷🇺🇸🇮🇱',
   'Me poem fora dessa meu chegado',
-  '😭🇨🇺🇻🇪😭'
+  'Duríssimo, duríssimo',
+  'Badaras'
 ]
 
 export {
